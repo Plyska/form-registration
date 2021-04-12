ec60e449a25240431fe7a1650a882d4ad233ef78
Андрій Плиска <andrijpliska@MacBook-Air-Andrij.local>
2021-04-13 01:24:29 +0300

    fixed bags

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -22,7 +23,11 @@ async function start() {
    });
 
    if (process.env.NODE_ENV === 'production') {
-     app.use(express.static("./client/build/"))
+     app.use(express.static("./client/build/"));
+
+     app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+     })
    }
 
    app.listen(PORT, () => {
