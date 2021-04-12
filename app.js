1665c9c3bdd83ad1e34cb2c85c026e6b22c8cb03
Андрій Плиска <andrijpliska@MacBook-Air-Andrij.local>
2021-04-13 01:26:56 +0300

    fixed some bags with deploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 
-app.use("/api/auth", require("./routes/auth.routes"))
+app.use("/api/auth", require("./routes/auth.routes"));
 
 //const PORT = config.get("port") || 5000;
 
@@ -15,24 +15,23 @@ const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-   await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    await mongoose.connect(process.env.MONGODB_URI || config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-   });
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static("./client/build/"));
 
-   if (process.env.NODE_ENV === 'production') {
-     app.use(express.static("./client/build/"));
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      });
+    }
 
-     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-     })
-   }
-
-   app.listen(PORT, () => {
-    console.log(`Apps has been started on port: ${PORT}...`);
-  });
+    app.listen(PORT, () => {
+      console.log(`Apps has been started on port: ${PORT}...`);
+    });
   } catch (e) {
     console.log("Server Error", e.message);
     process.exit(1);
